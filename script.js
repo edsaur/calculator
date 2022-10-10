084a7350e218fe31446f4bb6d126f5e682765a24
Ryan Vergara <6015315+rvvergara@users.noreply.github.com>
2022-10-10 17:16:55 +0800

    added new code

--- script.js
+++ script.js
@@ -1,26 +1,43 @@
+let display1 = {
+    evaluation: ""
+};
+
 function inputVal(val) {
-    let firstVal = $('#display1').val()
-    $('#display1').val(firstVal + val)
+    let firstVal = $('#display1').val();
+    $('#display1').val(firstVal + val);
+    $('#display2').val(firstVal + val);
+    $('#display2').val(eval($('#display1').val()))
 }
 
 function clearVal() {
-    $('#display1').val("")
-    $('#display2').val("")
+    $('#display1').val("");
+    $('#display2').val("");
+    
 }
 
+
+
 function calculate() {
-    let result = eval($('#display1').val())
-    $('#display2').val(result)
+    let result = eval($('#display1').val());
+    
+    $('#display2').val(result);
+    $('#display1').val(result)
+    $('#display2').val("")
 } 
+
+function answer() {
+$('#display1').val($('#display2').val());
+$('#display2').val("");
+
+}
 
 function erase() {
 
-    let presentNum = $('#display1').val()
+    let presentNum = $('#display1').val();
     if(presentNum !="") {
-       $('#display1').val($('#display1').val().slice(0, -1))
+       $('#display1').val($('#display1').val().slice(0, -1));
+       $('#display2').val(eval($('#display1').val()))
     }
-
-    $('#display2').val("")
 }
 
 
